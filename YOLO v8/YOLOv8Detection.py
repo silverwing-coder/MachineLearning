dc749839584e5ea5c096cfc1cc15a53c4a218f22
Sangmork "SAM" Park <sangmork.park@gmail.com>
2024-11-16 21:16:49 -0500

    UPDATE

--- YOLO v8/YOLOv8Detection.py
+++ YOLO v8/YOLOv8Detection.py
@@ -1,82 +1,58 @@
 import cv2
 from ultralytics import YOLO
+from omegaconf import OmegaConf
 
-CONF_LEVEL = 0.5
 
-### Load the YOLOv8 model
-model = YOLO("./models/yolov8n.pt")
+# Load the YOLOv8 model
+model = YOLO("./models/yolov8n-pose.pt")    
+# model = YOLO("./models/yolov8n-face.pt")
+# model = YOLO("./models/yolov8n")
 
-### print out all object classes in pre-trained model
-# print(model.names)
+def RunOnVideoClip():
 
-### method for model() results display
-def getColors(color_num):
-    base_colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
-    color_index = color_num % len(base_colors)
-    increments = [(1, -2, 1), (-2, 1, -1), (1, -1, 2)]
-    color = [base_colors[color_index][i] + increments[color_index][i] * 
-            (color_num // len(base_colors)) % 256 for i in range(3)]
-    return tuple(color)
+    # Source image
+    source = "./videos/video.mp4"
 
-### Main method
-def RunYoloObjectDetection():
+    # "save = True" will create folders and will save the result video
+    # at "./runs/pose/predict/file_name"
+    model.predict(source, save=True, imgsz=640, conf=0.5)
 
-    ### capture web-cam videl (laptop)
+
+
+def RunOnWebcam():
+    # Capture video camera (laptop)
     cap = cv2.VideoCapture(0)
-   
-    ### Loop through the video frames
+
+    # Loop through the video frames
     while (cap.isOpened()):
 
-        ### Load a frame from the capture
+        # Load a frame from the capture
         success, frame = cap.read()
 
         if success:
-            # Detect person class: classes = 0
-            # No output on screen: verbose = False
-            
-            ### detect all classes (1): model()
-            results = model(frame, stream=True)
-            
-            ### detect all classes (2): model.detect()
-            # results = model.predict(frame, save=False, conf=CONF_LEVEL, verbose=False)
+            # Run YOLO model and get the pose estimation data from the frame
+            results = model.predict(frame, save=False, imgsz=640, conf=0.5)
+            # print(results[0].keypoints.xyn)
 
-            ### detect person class only
-            # results = model.predict(frame, save=False, imgsz=640, conf=0.5, classes=0, verbose=False)
-            # print(results)
-            
-            ### display detected objects by default 
-            # for result in results:
-            #     annotated_frame = result.plot()
+            # Extract keypoint
+            result_keypoint = results[0].keypoints.xyn.cpu().numpy()[0]
+            # print(result_keypoint)
 
-            ### display detected objects by customized loop
-            for result in results:
-                class_names = result.names
-                for box in result.boxes:
-                    if(box.conf > CONF_LEVEL):
-                        [x1, y1, x2, y2] = box.xyxy[0]
-                        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            # Visualize the results on the frame
+            annotated_frame = results[0].plot()
 
-                        class_id = int(box.cls[0])
-                        # class_name = class_names[class_id]
-                        class_color = getColors(class_id)
-
-                        cv2.rectangle(frame, (x1, y1), (x2, y2), class_color, 2)
-                        cv2.putText(frame, f'{class_names[int(box.cls[0])]} {box.conf[0]:.2f}', 
-                                    (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1, class_color, 2) 
-
-
-            cv2.imshow("YOLO DETECTION", frame)
+            # Display the annotated frames
+            cv2.imshow("YOLO v8 Pose", annotated_frame)
 
             if (cv2.waitKey(1) == ord('q')):
                 break
         else:
             break
 
-    ### Release the resources and close all windows
+    # Release the resources and close all windows
     cap.release()
     cv2.destroyAllWindows()
 
-
 if __name__ == '__main__':
 
-    RunYoloObjectDetection()
+    RunOnWebcam()
--- YOLO v8/YOLOv8Pose.py
+++ YOLO v8/YOLOv8Pose.py
@@ -1,47 +1,47 @@
 import cv2
 from ultralytics import YOLO
-from omegaconf import OmegaConf
+# from omegaconf import OmegaConf
 
 
-# Load the YOLOv8 model
+### Load the YOLOv8 model
 model = YOLO("./models/yolov8n-pose.pt")    
-# model = YOLO("./models/yolov8n-face.pt")
-# model = YOLO("./models/yolov8n")
 
 def RunOnVideoClip():
 
-    # Source image
+    ### Source image
     source = "./videos/video.mp4"
 
-    # "save = True" will create folders and will save the result video
-    # at "./runs/pose/predict/file_name"
+    ### "save = True" will create folders and will save the result video
+    ### at "./runs/pose/predict/file_name"
     model.predict(source, save=True, imgsz=640, conf=0.5)
 
 
 
 def RunOnWebcam():
-    # Capture video camera (laptop)
+
+    ### capture video camera (laptop)
     cap = cv2.VideoCapture(0)
 
-    # Loop through the video frames
+    ### loop through the video frames
     while (cap.isOpened()):
 
-        # Load a frame from the capture
+        ### load a frame from the capture
         success, frame = cap.read()
 
         if success:
-            # Run YOLO model and get the pose estimation data from the frame
+            
+            ### run YOLO model and get the pose estimation data from the frame
             results = model.predict(frame, save=False, imgsz=640, conf=0.5)
             # print(results[0].keypoints.xyn)
 
-            # Extract keypoint
+            ### extract keypoint
             result_keypoint = results[0].keypoints.xyn.cpu().numpy()[0]
             # print(result_keypoint)
 
-            # Visualize the results on the frame
+            ### visualize the results on the frame
             annotated_frame = results[0].plot()
 
-            # Display the annotated frames
+            ### display the annotated frames
             cv2.imshow("YOLO v8 Pose", annotated_frame)
 
             if (cv2.waitKey(1) == ord('q')):
@@ -49,7 +49,7 @@ def RunOnWebcam():
         else:
             break
 
-    # Release the resources and close all windows
+    ### Release the resources and close all windows
     cap.release()
     cv2.destroyAllWindows()
 
